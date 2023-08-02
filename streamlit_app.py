a2a049a1205189c296fb7b6a577e03f4f4207f84
Akash Patel <venzuakash4225@gmail.com>
2023-08-02 14:07:29 -0600

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 
-streamlit.title("I love playing volleyball")
-streamlit.text("My fav part is serving")
+streamlit.title("I love playing volleyball. But first let's talk about breakfast")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
