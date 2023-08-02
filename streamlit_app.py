f8181fae9d75d9e9171756fc5bf1abb0dc7c4de6
Akash Patel <venzuakash4225@gmail.com>
2023-08-02 14:08:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("I love playing volleyball. But first let's talk about breakfast")
+streamlit.title("My Girlfriend's new Health Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
